072763bad56d5321be91c06ae36c2afa4a1e318a
Juliana Rodrigues Nogueira <114073784+rodrigues28ju@users.noreply.github.com>
2022-09-30 09:18:17 -0300

    Add files via upload

---
+++ 3006/dados.js
@@ -0,0 +1,51 @@
+var nome = "  juliana ";
+var sobrenome = " nogueira ";
+var nomeCompleto =  nome  +  sobrenome;
+
+ console.log (nomeCompleto);
+
+
+ var nome2 = "Juliana";
+ var sobrenome2 = "Rodrigues";
+ var nomeTotal = nome2 + ' ' + sobrenome2;
+
+ console.log(nomeTotal);
+
+//somando numeros com string
+ var gols = 1000;
+ var frase = "Coutinho fez " +  gols +"gols";
+ console.log(frase);
+
+
+ //utiizando template string sempre usa aspas invertidas
+
+ var gols = 1000;
+ var frases2 = `Coutinho fez ${gols*2} gols`;
+ console.log (frases2);
+
+
+
+
+ var letra = "oi";
+ console.log(letra);
+
+ var numeroLetra = "7 juliana"
+console.log(numeroLetra);
+
+var idade = 27;
+console.log(idade);
+
+
+var nome3 ="juliana";
+var sobrenome3 = "Rodrigues Nogueira";
+var totalNome = nome3 + ' ' + sobrenome3;
+console.log( typeof nome3);
+console.log (totalNome);
+
+var frase3 = "It`s time";
+console.log(frase3);
+
+
+
+
+
